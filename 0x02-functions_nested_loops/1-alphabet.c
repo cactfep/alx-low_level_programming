2696704c93c014c25a850cac61392714001b76fc
cactfep <mattbggg@gmail.com>
2022-10-19 03:14:43 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print lowercase alphabets
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+}
