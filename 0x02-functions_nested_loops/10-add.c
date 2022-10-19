b8f77bf38064c5eb12700019bb3b66f636716eb9
cactfep <mattbggg@gmail.com>
2022-10-19 04:09:05 -0700

    sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @a: right operand
+ * @b: left operand
+ * Return: the sum
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
