3af54b4941e5f1e33e1c63a21abe601946258d5a
cactfep <mattbggg@gmail.com>
2022-10-19 03:48:24 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _abs - absolute value of an iteger
+ * @n: integer
+ * Return: integer
+ */
+
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
