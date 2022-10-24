3bfa22110e7e50d2f60c90ee80c1aa12f8cad44e
cactfep <mattbggg@gmail.com>
2022-10-24 10:30:55 -0700

    length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -9,11 +9,12 @@
 int _strlen(char *s)
 {
 	int count, inc;
-
 	inc = 0;
 
-	for (count = 0; s[count] != '\0'; count++;)
+	for (count = 0; s[count] != '\0'; count++)
+	{
 		inc++;
+	}
 
 	return (inc);
 }
