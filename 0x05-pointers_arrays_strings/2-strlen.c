280851e7c30bf29691014ec082987aadc4860aac
cactfep <mattbggg@gmail.com>
2022-10-24 10:22:38 -0700

    length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.c"
+
+/**
+ * _strlen - length of the string
+ * @s: chracter parameter
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int count, inc;
+
+	inc = 0;
+
+	for (count = 0, s[count] != '\0'; count++)
+		inc++;
+
+	return (inc);
+}
